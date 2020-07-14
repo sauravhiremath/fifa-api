4c715d231166e65dcfbfa761f62bfc22b479f10e
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-07-14 09:03:54 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ import cors from 'cors';
 
 import { socker } from './socker';
 import { handleError, authenticated, logger } from './middlewares';
-import { API_PORT, host } from './env';
+import { API_PORT, hosts } from './env';
 
 const app = express();
 const server = new http.Server(app);
 socker(server);
 
-app.use(cors({ origin: host, credentials: true }));
+app.use(cors({ origin: hosts, credentials: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/users', authenticated);
@@ -31,5 +31,5 @@ app.listen(API_PORT, () => {
 
 server.listen(Number(API_PORT) + 1, () => {
     logger.info(`Socker listening on port ${Number(API_PORT) + 1}!`);
-    logger.info(`Api and socker whitelisted for ${host}`);
+    logger.info(`Api and socker whitelisted for ${hosts}`);
 });
