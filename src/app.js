d378c76a94eec34dff2a7998df6ffee6a3fcf8b2
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-12-29 16:30:20 +0530

    fix: remove body-parser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import './database/db';
 import express from 'express';
 import http from 'http';
-import bodyParser from 'body-parser';
 import routes from './routes';
 import cors from 'cors';
 import consola from 'consola';
@@ -15,8 +14,7 @@ const server = new http.Server(app);
 socker(server);
 
 app.use(cors({ origin: hosts, credentials: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/users', authenticated);
 app.use('/search', authenticated);
 
@@ -27,10 +25,10 @@ app.use((err, _req, res, _) => {
 });
 
 app.listen(API_PORT, () => {
-    consola.info(`Api listening on port ${Number(API_PORT)}!`);
+    consola.success(`Api listening on port ${Number(API_PORT)}!`);
 });
 
 server.listen(Number(API_PORT) + 1, () => {
-    consola.info(`Socker listening on port ${Number(API_PORT) + 1}!`);
+    consola.success(`Socker listening on port ${Number(API_PORT) + 1}!`);
     consola.info(`Api and socker whitelisted for ${hosts}`);
 });
